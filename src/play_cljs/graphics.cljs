d8dcc35510dfa068c0dae60a04a207023ef64512
oakes <zsoakes@gmail.com>
2016-07-27 18:53:05 -0400

    Allow putting shapes inside other shapes

--- src/play_cljs/graphics.cljs
+++ src/play_cljs/graphics.cljs
@@ -13,36 +13,53 @@
 
 (defmethod draw-graphics! :circle [command origin-x origin-y graphics]
   (let [[_ opts & children] command
-        {:keys [x y radius]} opts]
-    (.drawCircle graphics (+ origin-x x) (+ origin-y y) radius)))
+        {:keys [x y radius]} opts
+        x (+ origin-x x)
+        y (+ origin-y y)]
+    (.drawCircle graphics x y radius)
+    (draw-graphics! children x y graphics)))
 
 (defmethod draw-graphics! :ellipse [command origin-x origin-y graphics]
   (let [[_ opts & children] command
-        {:keys [x y width height]} opts]
-    (.drawEllipse graphics (+ origin-x x) (+ origin-y y) width height)))
+        {:keys [x y width height]} opts
+        x (+ origin-x x)
+        y (+ origin-y y)]
+    (.drawEllipse graphics x y width height)
+    (draw-graphics! children x y graphics)))
 
 (defmethod draw-graphics! :polygon [command origin-x origin-y graphics]
   (let [[_ opts & children] command
-        {:keys [path]} opts]
-    (let [path (->> path
-                    (partition 2)
-                    (map (fn [[x y]] [(+ origin-x x) (+ origin-y y)]))
-                    flatten)]
-      (.drawPolygon graphics (into-array path)))))
+        {:keys [path]} opts
+        path (->> path
+                  (partition 2)
+                  (map (fn [[x y]] [(+ origin-x x) (+ origin-y y)]))
+                  flatten)]
+    (.drawPolygon graphics (into-array path))
+    (draw-graphics! children origin-x origin-y graphics)))
 
 (defmethod draw-graphics! :rect [command origin-x origin-y graphics]
   (let [[_ opts & children] command
-        {:keys [x y width height radius]} opts]
-    (.drawRect graphics (+ origin-x x) (+ origin-y y) width height)))
+        {:keys [x y width height radius]} opts
+        x (+ origin-x x)
+        y (+ origin-y y)]
+    (.drawRect graphics x y width height)
+    (draw-graphics! children x y graphics)))
 
 (defmethod draw-graphics! :rounded-rect [command origin-x origin-y graphics]
   (let [[_ opts & children] command
-        {:keys [x y width height radius]} opts]
-    (.drawRoundedRect graphics (+ origin-x x) (+ origin-y y) width height radius)))
+        {:keys [x y width height radius]} opts
+        x (+ origin-x x)
+        y (+ origin-y y)]
+    (.drawRoundedRect graphics x y width height radius)
+    (draw-graphics! children x y graphics)))
 
 (defmethod draw-graphics! :default [command origin-x origin-y graphics]
-  (if (sequential? (first command))
+  (cond
+    (sequential? (first command))
     (run! #(draw-graphics! % origin-x origin-y graphics) command)
+    (nil? (first command))
+    nil
+    :else
     (throw (js/Error. (str "Invalid graphics command: " (pr-str command))))))
 
 (defrecord Graphics [command x y] c/Command
