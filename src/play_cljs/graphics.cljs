8a60e2e261dece61310160178d93b3eda1b59b0d
oakes <zsoakes@gmail.com>
2016-07-27 18:40:42 -0400

    Allow setting the origin of the graphics object

--- src/play_cljs/graphics.cljs
+++ src/play_cljs/graphics.cljs
@@ -2,52 +2,59 @@
   (:require [cljsjs.pixi]
             [play-cljs.core :as c]))
 
-(defmulti draw-graphics! (fn [command graphics] (first command)))
+(defmulti draw-graphics! (fn [command _ _ _] (first command)))
 
-(defmethod draw-graphics! :fill [command graphics]
+(defmethod draw-graphics! :fill [command origin-x origin-y graphics]
   (let [[_ opts & children] command
         {:keys [color alpha]} opts]
     (.beginFill graphics color alpha)
-    (draw-graphics! children graphics)
+    (draw-graphics! children origin-x origin-y graphics)
     (.endFill graphics)))
 
-(defmethod draw-graphics! :circle [command graphics]
+(defmethod draw-graphics! :circle [command origin-x origin-y graphics]
   (let [[_ opts & children] command
         {:keys [x y radius]} opts]
-    (.drawCircle graphics x y radius)))
+    (.drawCircle graphics (+ origin-x x) (+ origin-y y) radius)))
 
-(defmethod draw-graphics! :ellipse [command graphics]
+(defmethod draw-graphics! :ellipse [command origin-x origin-y graphics]
   (let [[_ opts & children] command
         {:keys [x y width height]} opts]
-    (.drawEllipse graphics x y width height)))
+    (.drawEllipse graphics (+ origin-x x) (+ origin-y y) width height)))
 
-(defmethod draw-graphics! :polygon [command graphics]
+(defmethod draw-graphics! :polygon [command origin-x origin-y graphics]
   (let [[_ opts & children] command
         {:keys [path]} opts]
-    (.drawPolygon graphics (into-array path))))
+    (let [path (->> path
+                    (partition 2)
+                    (map (fn [[x y]] [(+ origin-x x) (+ origin-y y)]))
+                    flatten)]
+      (.drawPolygon graphics (into-array path)))))
 
-(defmethod draw-graphics! :rect [command graphics]
+(defmethod draw-graphics! :rect [command origin-x origin-y graphics]
   (let [[_ opts & children] command
         {:keys [x y width height radius]} opts]
-    (.drawRect graphics x y width height)))
+    (.drawRect graphics (+ origin-x x) (+ origin-y y) width height)))
 
-(defmethod draw-graphics! :rounded-rect [command graphics]
+(defmethod draw-graphics! :rounded-rect [command origin-x origin-y graphics]
   (let [[_ opts & children] command
         {:keys [x y width height radius]} opts]
-    (.drawRoundedRect graphics x y width height radius)))
+    (.drawRoundedRect graphics (+ origin-x x) (+ origin-y y) width height radius)))
 
-(defmethod draw-graphics! :default [command graphics]
+(defmethod draw-graphics! :default [command origin-x origin-y graphics]
   (if (sequential? (first command))
-    (run! #(draw-graphics! % graphics) command)
+    (run! #(draw-graphics! % origin-x origin-y graphics) command)
     (throw (js/Error. (str "Invalid graphics command: " (pr-str command))))))
 
-(defrecord Graphics [command] c/Command
+(defrecord Graphics [command x y] c/Command
   (run [this game]
     (let [renderer (c/get-renderer game)
           graphics (js/PIXI.Graphics.)]
-      (draw-graphics! command graphics)
+      (draw-graphics! command x y graphics)
       (.render renderer graphics))))
 
-(defn graphics [command]
-  (Graphics. command))
+(defn graphics
+  ([command]
+   (graphics command 0 0))
+  ([command x y]
+   (Graphics. command x y)))
 
