b7c00054543474f5c8d0f51fe76ab8b71bd8d371
oakes <zsoakes@gmail.com>
2016-08-07 16:01:06 -0400

    Finish text

--- src/play_cljs/sketch.cljs
+++ src/play_cljs/sketch.cljs
@@ -7,22 +7,26 @@
                              :text
                              command))))
 
-(def ^:const text-defaults {:x 0 :y 0 :size 32 :font "Helvetica" :halign :left :valign :baseline})
+(def ^:const text-defaults {:x 0 :y 0 :size 32 :font "Helvetica" :halign :left :valign :baseline :leading 0 :style :normal})
 (defn halign->constant [renderer halign]
   (get {:left (.-LEFT renderer) :center (.-CENTER renderer) :right (.-RIGHT renderer)} halign))
 (defn valign->constant [renderer valign]
   (get {:top (.-TOP renderer) :center (.-CENTER renderer) :bottom (.-BOTTOM renderer) :baseline (.-BASELINE renderer)} valign))
+(defn style->constant [renderer style]
+  (get {:normal (.-NORMAL renderer) :italic (.-ITALIC renderer) :bold (.-BOLD renderer)} style))
 
 (defmethod draw-sketch! :text [renderer content parent-opts]
   (let [[command opts & children] content
         parent-opts (merge text-defaults parent-opts)
-        opts (-> (merge text-defaults (select-keys parent-opts [:size :font :halign :valign]) opts)
+        opts (-> (merge text-defaults (dissoc parent-opts :x :y) opts)
                  (update :x + (:x parent-opts))
                  (update :y + (:y parent-opts)))
-        {:keys [x y size font halign valign]} opts]
+        {:keys [x y size font halign valign leading style]} opts]
     (.textSize renderer size)
     (.textFont renderer font)
     (.textAlign renderer (halign->constant renderer halign) (valign->constant renderer valign))
+    (.textLeading renderer leading)
+    (.textStyle renderer (style->constant renderer style))
     (.text renderer command x y)
     (draw-sketch! renderer children opts)))
 
