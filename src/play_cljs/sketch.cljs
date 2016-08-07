7c9f1fd10cb311176b06fe754cf84cf4dc41719d
oakes <zsoakes@gmail.com>
2016-08-07 16:43:25 -0400

    Add more shapes

--- src/play_cljs/sketch.cljs
+++ src/play_cljs/sketch.cljs
@@ -51,12 +51,55 @@
 (defmethod draw-sketch! :line [renderer content parent-opts]
   (let [[command opts & children] content
         opts (update-opts opts parent-opts basic-defaults)
-        {:keys [x1 y1 x2 y2] :as opts} (-> opts
-                                           (update :x1 + (:x opts))
-                                           (update :x2 + (:x opts))
-                                           (update :y1 + (:y opts))
-                                           (update :y2 + (:y opts)))]
+        {:keys [x1 y1 x2 y2] :as opts}
+        (-> opts
+            (update :x1 + (:x opts))
+            (update :x2 + (:x opts))
+            (update :y1 + (:y opts))
+            (update :y2 + (:y opts)))]
     (.line renderer x1 y1 x2 y2)
+    (draw-sketch! renderer children opts)))
+
+(defmethod draw-sketch! :point [renderer content parent-opts]
+  (let [[command opts & children] content
+        {:keys [x y] :as opts} (update-opts opts parent-opts basic-defaults)]
+    (.point renderer x y)
+    (draw-sketch! renderer children opts)))
+
+(defmethod draw-sketch! :quad [renderer content parent-opts]
+  (let [[command opts & children] content
+        opts (update-opts opts parent-opts basic-defaults)
+        {:keys [x1 y1 x2 y2 x3 y3 x4 y4] :as opts}
+        (-> opts
+            (update :x1 + (:x opts))
+            (update :x2 + (:x opts))
+            (update :y1 + (:y opts))
+            (update :y2 + (:y opts))
+            (update :x3 + (:x opts))
+            (update :y3 + (:y opts))
+            (update :x4 + (:x opts))
+            (update :y4 + (:y opts)))]
+    (.quad renderer x1 y1 x2 y2 x3 y3 x4 y4)
+    (draw-sketch! renderer children opts)))
+
+(defmethod draw-sketch! :rect [renderer content parent-opts]
+  (let [[command opts & children] content
+        {:keys [x y width height] :as opts} (update-opts opts parent-opts basic-defaults)]
+    (.rect renderer x y width height)
+    (draw-sketch! renderer children opts)))
+
+(defmethod draw-sketch! :triangle [renderer content parent-opts]
+  (let [[command opts & children] content
+        opts (update-opts opts parent-opts basic-defaults)
+        {:keys [x1 y1 x2 y2 x3 y3] :as opts}
+        (-> opts
+            (update :x1 + (:x opts))
+            (update :x2 + (:x opts))
+            (update :y1 + (:y opts))
+            (update :y2 + (:y opts))
+            (update :x3 + (:x opts))
+            (update :y3 + (:y opts)))]
+    (.triangle renderer x1 y1 x2 y2 x3 y3)
     (draw-sketch! renderer children opts)))
 
 (defmethod draw-sketch! :default [renderer content parent-opts]
