2fa7204971ef1d0b348fcc1a6f0d657a0b66f2d8
oakes <zsoakes@gmail.com>
2016-08-07 16:05:37 -0400

    Refactor

--- src/play_cljs/sketch.cljs
+++ src/play_cljs/sketch.cljs
@@ -1,27 +1,32 @@
 (ns play-cljs.sketch
   (:require [p5.core]))
 
+(defn update-opts [opts parent-opts defaults]
+  (let [parent-opts (merge defaults parent-opts)]
+    (-> (merge defaults (dissoc parent-opts :x :y) opts)
+        (update :x + (:x parent-opts))
+        (update :y + (:y parent-opts)))))
+
+(def ^:const text-defaults {:x 0 :y 0 :size 32 :font "Helvetica" :halign :left :valign :baseline :leading 0 :style :normal})
+
+(defn halign->constant [renderer halign]
+  (get {:left (.-LEFT renderer) :center (.-CENTER renderer) :right (.-RIGHT renderer)} halign))
+
+(defn valign->constant [renderer valign]
+  (get {:top (.-TOP renderer) :center (.-CENTER renderer) :bottom (.-BOTTOM renderer) :baseline (.-BASELINE renderer)} valign))
+
+(defn style->constant [renderer style]
+  (get {:normal (.-NORMAL renderer) :italic (.-ITALIC renderer) :bold (.-BOLD renderer)} style))
+
 (defmulti draw-sketch! (fn [renderer content parent-opts]
                          (let [command (first content)]
                            (if (string? command)
                              :text
                              command))))
 
-(def ^:const text-defaults {:x 0 :y 0 :size 32 :font "Helvetica" :halign :left :valign :baseline :leading 0 :style :normal})
-(defn halign->constant [renderer halign]
-  (get {:left (.-LEFT renderer) :center (.-CENTER renderer) :right (.-RIGHT renderer)} halign))
-(defn valign->constant [renderer valign]
-  (get {:top (.-TOP renderer) :center (.-CENTER renderer) :bottom (.-BOTTOM renderer) :baseline (.-BASELINE renderer)} valign))
-(defn style->constant [renderer style]
-  (get {:normal (.-NORMAL renderer) :italic (.-ITALIC renderer) :bold (.-BOLD renderer)} style))
-
 (defmethod draw-sketch! :text [renderer content parent-opts]
   (let [[command opts & children] content
-        parent-opts (merge text-defaults parent-opts)
-        opts (-> (merge text-defaults (dissoc parent-opts :x :y) opts)
-                 (update :x + (:x parent-opts))
-                 (update :y + (:y parent-opts)))
-        {:keys [x y size font halign valign leading style]} opts]
+        {:keys [x y size font halign valign leading style] :as opts} (update-opts opts parent-opts text-defaults)]
     (.textSize renderer size)
     (.textFont renderer font)
     (.textAlign renderer (halign->constant renderer halign) (valign->constant renderer valign))
