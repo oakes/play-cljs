24145780e3de96fb659edd1710343fd9b25b65c1
oakes <zach@sekao.net>
2019-02-25 07:21:11 -0600

    Update template

--- template/src/leiningen/new/play_cljs.clj
+++ template/src/leiningen/new/play_cljs.clj
@@ -12,31 +12,36 @@
         (remove empty? $)
         (str/join "-" $)))
 
+(defn play-cljs-data [name]
+  (let [[project-name core-name] (str/split name #"\." 2)
+        project-name (sanitize-name project-name)
+        core-name (if core-name (sanitize-name core-name) "core")]
+    (when (or (not (seq project-name))
+                   (not (seq core-name)))
+      (throw (Exception. (str "Invalid name: " name))))
+    {:name project-name
+     :core-name core-name
+     :project_name (str/replace project-name "-" "_")
+     :core_name (str/replace core-name "-" "_")}))
+
+(defn play-cljs*
+  [{:keys [project_name core_name] :as data}]
+  (let [render (t/renderer "play-cljs")]
+    {"README.md" (render "README.md" data)
+     ".gitignore" (render "gitignore" data)
+     "deps.edn" (render "deps.edn" data)
+     "figwheel-main.edn" (render "figwheel-main.edn" data)
+     "dev.cljs.edn" (render "dev.cljs.edn" data)
+     "dev.clj" (render "dev.clj" data)
+     "prod.clj" (render "prod.clj" data)
+     (str "src/" project_name "/music.clj") (render "music.clj" data)
+     (str "src/" project_name "/" core_name ".cljs") (render "core.cljs" data)
+     (str "src/" project_name "/dev.cljs") (render "dev.cljs" data)
+     "resources/public/index.html" (render "index.html" data)}))
+
 (defn play-cljs
   [name & _]
-  (let [render (t/renderer "play-cljs")
-        [project-name core-name] (str/split name #"\." 2)
-        project-name (sanitize-name project-name)
-        core-name (if core-name (sanitize-name core-name) "core")
-        _ (when (or (not (seq project-name))
-                    (not (seq core-name)))
-            (throw (Exception. (str "Invalid name: " name))))
-        data {:name project-name
-              :core-name core-name
-              :project_name (str/replace project-name "-" "_")
-              :core_name (str/replace core-name "-" "_")}]
-    (when (= core-name "music")
-      (throw (Exception. "Please choose a different name than 'music'")))
-    (t/->files data
-      ["README.md" (render "README.md" data)]
-      [".gitignore" (render "gitignore" data)]
-      ["deps.edn" (render "deps.edn" data)]
-      ["figwheel-main.edn" (render "figwheel-main.edn" data)]
-      ["dev.cljs.edn" (render "dev.cljs.edn" data)]
-      ["dev.clj" (render "dev.clj" data)]
-      ["prod.clj" (render "prod.clj" data)]
-      ["src/{{project_name}}/{{core_name}}.cljs" (render "core.cljs" data)]
-      ["src/{{project_name}}/music.clj" (render "music.clj" data)]
-      ["src/{{project_name}}/dev.cljs" (render "dev.cljs" data)]
-      ["resources/public/index.html" (render "index.html" data)])))
+  (let [data (play-cljs-data name)
+        path->content (play-cljs* data)]
+    (apply t/->files data (vec path->content))))
 
